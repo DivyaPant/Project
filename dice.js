e5ee778b2d9c3a10b12d969d0a83ebe92cd5362c
Divya Pant <60366515+DivyaPant@users.noreply.github.com>
2020-06-09 16:08:16 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -17,12 +17,12 @@ document.querySelector(".dice .img2").setAttribute("src", randomImage2);
 
 if (randomNumber1>randomNumber2) {
 
-	document.querySelector("h1").innerHTML= "🚩 Play 1 Wins!";
+	document.querySelector("h1").innerHTML= "🚩 Player 1 Wins!";
 }
 
 else if(randomNumber1<randomNumber2) {
 
-	document.querySelector("h1").innerHTML= "Play 2 Wins! 🚩";
+	document.querySelector("h1").innerHTML= "Player 2 Wins! 🚩";
 }
 
 else {
